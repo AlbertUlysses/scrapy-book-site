73be9965febdd536b0bed3c36f490123ee8339b5
AlbertUlysses <47486070+AlbertUlysses@users.noreply.github.com>
2019-05-12 21:22:27 -0700

    Add Comments
    Added a few comments.

--- bookSite/bookSite/spiders/bookSpider.py
+++ bookSite/bookSite/spiders/bookSpider.py
@@ -6,11 +6,12 @@ class BookSpider(scrapy.Spider):
 
     start_urls = ["http://books.toscrape.com/"]
 
-    
+#spider that yields the book categories and links from the mainpage    
     def parse(self, response):
+        #enumerates using index to prevent yielding the same value 50 times 
         for index, book in enumerate(response.xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul/li/a')):
             yield {
-                    'text': book.xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul/li/a/text()')[index].get().strip(),
+                    'category': book.xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul/li/a/text()')[index].get().strip(),
                     'link': book.xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul/li/a/@href')[index].get()
                 }
-        #next_page = response.xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul/li[1]/a/@href').get()
+        
